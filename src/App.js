42b1808bd789853782574303f42bbe609b1cce68
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-12 19:10:21 +0900

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { CreateContainer, MainContainer } from './components';
 import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-primary">
       <Header />
+
+      <main className="mt-24 p-8 w-full">
+        <Routes>
+          <Route path="/*" element={<MainContainer />} />
+          <Route path="/createItem" element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md';
 
+import Logo from './img/logo.png';
+import Avatar from './img/avatar.png';
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+    <header className="fixed z-50 w-screen p-6 px-16">
         {/* desktop & tablet */}
-        <div className="hidden md:flex w-full h-full">
+        <div className="hidden md:flex w-full h-full items-center justify-between">
+            <div className="flex items-center gap-2">
+                <img src={Logo} className="w-8 object-cover" alt="logo" />
+                <p className="text-headingColor text-xl font-bold">REST DELIVERY</p>
+            </div>
 
+            <div className="flex items-center gap-8">
+                <ul className="flex items-center gap-8">
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">홈</li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">메뉴</li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">우리에 대해서</li>
+                    <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">서비스</li>
+                </ul>
+                <div className="relative flex items-center justify-center">
+                    <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+                    <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                        <p className="text-xs text-white font-semibold">2</p>
+                    </div>
+                </div>
+
+                <img
+                    src={Avatar}
+                    className="w-10 min-w-[40px] h-10 min-h-[40px] shadow-2xl drop-shadow-xl"
+                    alt="userprofile"
+                />
+            </div>
         </div>
 
         {/* mobile */}
         <div className="flex md:hidden w-full"></div>
-    </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
