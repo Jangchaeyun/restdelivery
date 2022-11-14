10b2e59d187a056f8b5d87dac1e3550bf96d88f4
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-14 21:42:16 +0900

    Home Container_3

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png";
+import F1 from '../img/f1.png';
+import F2 from '../img/f2.png';
+import F3 from '../img/f3.png';
+
+export const heroData = [
+    {
+        id: 1,
+        name: '밭빙수',
+        decp: '빙수',
+        price: '7,500',
+        imageSrc: I1
+    }, 
+    {
+        id: 2,
+        name: '유부우동',
+        decp: '우동',
+        price: '22,000',
+        imageSrc: F1
+    }, 
+    {
+        id: 3,
+        name: '양지쌀국수',
+        decp: '국수',
+        price: '8,500',
+        imageSrc: F2
+    }, 
+    {
+        id: 4,
+        name: '김치찌게',
+        decp: '찌게',
+        price: '9,000',
+        imageSrc: F3
+    }
+]
