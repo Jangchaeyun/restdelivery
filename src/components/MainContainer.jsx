0421e6c8232583030cf3ef21405f0a5ad8645e7d
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-17 21:01:30 +0900

    Cart UI Component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,6 +5,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md"
 import RowContainer from './RowContainer';
 import { useStateValue } from '../context/StateProvider';
 import MenuContainer from './MenuContainer';
+import CartContainer from './CartContainer';
 
 const MainContainer = () => {
   const [{foodItems}, dispatch] = useStateValue();
@@ -45,7 +46,10 @@ const MainContainer = () => {
           data = {foodItems?.filter(n => n.category === "korean")} 
         />
       </section>
+
       <MenuContainer />
+
+      <CartContainer />
     </div>
   );
 };
